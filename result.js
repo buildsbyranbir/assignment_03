95f9174e3343773d6f676c52fd28c0f25a4a2734
buildsbyranbir <ranbir.webdev@gmail.com>
2025-08-03 23:13:46 +0600

    Added all changes

--- result.js
+++ result.js
@@ -1,5 +1,5 @@
 /** Problem -01 ( Divide the Asset ) */
-var area = 800;
+var area = 2060;
 //write your code here
 var division = area / 2;
 console.log (division)
@@ -9,7 +9,6 @@ console.log (division)
 /** Problem -02 ( Cycle or Laptop ) */
 var money = 10000;
 //write your code here
-
 if (money >= 25000) {
     console.log("Laptop");
 } else if (money >= 10000) {
@@ -23,7 +22,6 @@ if (money >= 25000) {
 /** Problem -03 ( Medicine Planner ) */
 var lastDay = 11 ;
 //write your code here
-
 for (var day = 1; day <= lastDay; day++) {
     if (day % 3 === 0) {
         console.log(day + " - medicine");
@@ -37,8 +35,7 @@ for (var day = 1; day <= lastDay; day++) {
 /** Problem 04 - (Delete / Store) */
 var fileName= "data.docx";
 //write your code here
-
-if (fileName.startsWith("#") || fileName.endsWith(".pdf") || fileName.endsWith(".docx")) {
+if (fileName[0] === '#' || fileName.slice(-4) === ".pdf" || fileName.slice(-5) === ".docx") {
     console.log("Store");
 } else {
     console.log("Delete");
@@ -49,7 +46,6 @@ if (fileName.startsWith("#") || fileName.endsWith(".pdf") || fileName.endsWith("
 /** Problem 05 - ( PH Email Generator )  */
 var student= { name: "jhanku" , roll: 1014 ,department: "cse" };
 //write your code here
-
 var email =student.name + student.roll + "." + student.department + "@ph.ac.bd";
 console.log(email);
 
@@ -58,9 +54,6 @@ console.log(email);
 var experience = 40;
 var startingSalary = 30000;
 //write your code here
-
 for (var i = 1; i <= experience; i++) {
-    startingSalary *= 1.05;
-}
-
+    startingSalary *= 1.05;}
 console.log(startingSalary.toFixed(2));
